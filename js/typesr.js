22b03578f60c9b578e039c4ef98bf825d4c1ff55
Bitstick <thebitstick@tfwno.gf>
2016-03-08 13:42:59 -0600

    Serbian

---
+++ js/typesr.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $("#file").change(function () 
+   { 
+     var ext = $('#file').val().split('.').pop().toLowerCase();
+     if($.inArray(ext, ['png','jpg','jpeg','gif','webm','txt','mp4','wmv','mp3','ogg','zip','css','doc','docx','odt','pdf','ass','mkv','psd','flac']) == -1)
+      {
+    	alert('Неподржан тип датотеке, молим прочитај услове.');
+        location.reload();
+} 
+  }); 
+});
